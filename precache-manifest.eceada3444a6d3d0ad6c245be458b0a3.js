2cad98bb14b5554a80ba007776c638b0144060fe
Camilo Barraza <cbarraza11personal@gmail.com>
2020-08-30 22:25:04 -0500

    Updates

---
+++ precache-manifest.eceada3444a6d3d0ad6c245be458b0a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe0f683f7394e6d5c60428799e12361",
+    "url": "/bandits_activity/index.html"
+  },
+  {
+    "revision": "6396c9a462cb8b68ec82",
+    "url": "/bandits_activity/static/css/2.6d4ecc00.chunk.css"
+  },
+  {
+    "revision": "965cf4596bb74f6f03dd",
+    "url": "/bandits_activity/static/css/main.d42672c2.chunk.css"
+  },
+  {
+    "revision": "6396c9a462cb8b68ec82",
+    "url": "/bandits_activity/static/js/2.d4b175c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bandits_activity/static/js/2.d4b175c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965cf4596bb74f6f03dd",
+    "url": "/bandits_activity/static/js/main.fb38f38b.chunk.js"
+  },
+  {
+    "revision": "d0a38806eb2b8eb69722",
+    "url": "/bandits_activity/static/js/runtime-main.8922f8b3.js"
+  },
+  {
+    "revision": "dabd52f31d0cd9551993d6862fdaa3bd",
+    "url": "/bandits_activity/static/media/jackpots-lose.dabd52f3.png"
+  },
+  {
+    "revision": "b1244348c0071326d5a8807270f4fedb",
+    "url": "/bandits_activity/static/media/jackpots-win.b1244348.png"
+  },
+  {
+    "revision": "f42bc570518bf11aa0f488a7e8188816",
+    "url": "/bandits_activity/static/media/jackpots-win.f42bc570.svg"
+  },
+  {
+    "revision": "26ce92e93c2d76d1a47e168b544a8099",
+    "url": "/bandits_activity/static/media/jackpots.26ce92e9.png"
+  }
+]);
