d84daf2222f1bfde21e132ba5e9c6d19b1a69403
Andea Barraza <Andrea@macs-MacBook-Pro.local>
2020-08-31 05:54:43 +0100

    Test title and instructions update

--- src/config.js
+++ src/config.js
@@ -1,14 +1,7 @@
 export const instructions = `
-  Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos
-  de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias
-  desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) 
-  desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un
-  libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
-  como texto de relleno en documentos electrónicos, quedando esencialmente
-  igual al original. Fue popularizado en los 60s con la creación de las hojas
-  "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientement
+  Try to get the most coins that you can!
 `
-export const TITLE = 'Slot Machine Game'
+export const TITLE = 'Multi-Armed Bandits Activity'
 
 export const LEVELS = {
   EASY: 'Easy',
